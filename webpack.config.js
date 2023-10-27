f13770e497a67a39a647aedd01840d87876e3c4a
Denis Nikolskii <den-apple@ya.ru>
2023-10-27 17:25:35 +0200

    Loaders

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,29 @@ module.exports = {
       {
         test: /\.txt/,
         type: 'asset/source'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [ '@babel/env' ],
+            plugins: [ '@babel/plugin-proposal-class-properties' ]
+          }
+        }
       }
     ]
   }
